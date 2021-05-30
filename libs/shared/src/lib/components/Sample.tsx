b5673db81514b59018f7681cd0f9cf6a95b7f6c7
Vaibhav Shringarpure <vaibhav.shringarpure@pvh.com>
2021-05-30 22:09:09 +0100

    refactor: renamed component to sample

---
+++ libs/shared/src/lib/components/Sample.tsx
@@ -0,0 +1,56 @@
+import styled from 'styled-components'
+import {Box, Button, Typography} from '@material-ui/core'
+import {withStyles, makeStyles} from '@material-ui/core/styles'
+import {ArrowForward} from '@material-ui/icons'
+import {Colour} from '@vam/shared/styles/theme/config'
+/* eslint-disable-next-line */
+export interface SampleProps {}
+
+const StyledComponents = styled.div`
+  color: #333;
+`
+const VAMButton = withStyles(() => ({
+  root: {
+    '&:hover': {
+      backgroundColor: Colour.hoverLinkColor,
+    },
+    '&:focus': {
+      backgroundColor: Colour.focusColor,
+      color: Colour.focusTextColor,
+    },
+  },
+}))(Button)
+
+const useStyles = makeStyles(() => ({
+  padding: {
+    padding: '20px',
+  },
+}))
+
+export function Sample(props: SampleProps) {
+  const classes = useStyles()
+  return (
+    <StyledComponents>
+      <Box {...{mb: 2}}>
+        <Typography
+          {...{
+            variant: 'h4',
+            component: 'h1',
+          }}
+        >
+          Welcome to the VAM Digital Starter Template
+        </Typography>
+      </Box>
+      <VAMButton
+        className={classes.padding}
+        color="secondary"
+        variant="contained"
+        endIcon={<ArrowForward />}
+      >
+        Mui Button
+      </VAMButton>
+    </StyledComponents>
+  )
+}
+
+export default Sample

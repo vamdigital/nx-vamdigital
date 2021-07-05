dd8835e2b499a6c5fe5040d0346af674dad5cb0b
VAM Digital <vamdigitalltd@gmail.com>
2021-07-05 16:49:00 +0100

    Removed console.log for downloading bit

--- bin/setup.js
+++ bin/setup.js
@@ -25,7 +25,6 @@ if (process.argv.slice(2).length > 0) {
 const folderPath = join(initWorkingDirectory, folderName)
 
 const repo = 'https://github.com/vamdigital/nx-vamdigital.git'
-console.log(`downloadin files from repo ${repo} 🚀`)
 
 const execPromise = promisify(exec)
 async function runShellCmd(command) {
